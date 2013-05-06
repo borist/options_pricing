ac94992f0a32ac029495eb4ccad4c84dd763fe6d
Boris Treskunov <borist@seas.upenn.edu>
2013-05-06 00:58:50 -0400

    initial commit

---
+++ black_scholes.py
@@ -0,0 +1,16 @@
+import math
+
+# -----------------------------------------
+# Variables used in B.S. and their meanings:
+# 
+# ------------------------------------------
+
+# Time steps
+one_month   = 1.0 / 12.0
+two_month   = 2.0 / 12.0
+one_month   = 3.0 / 12.0
+four_month  = 4.0 / 12.0
+six_month   = 6.0 / 12.0
+one_year    = 1.0
+
+
